6c46f12613bae8d488662c99862450e5207fc9c1
pearlynne <chongpearlynnelh@gmail.com>
2024-05-13 17:08:46 -0700

    Attempt DAOs for appointments

--- daos/appointment.js
+++ daos/appointment.js
@@ -1,9 +1,37 @@
-const appointment = require("../models/appointment")
+const Appointment = require("../models/appointment");
 
-// - **createAppt(userId, apptObj):** Users/Healthcare Providers can create new appointments. 
+module.exports = {};
 
-// - **getAppt(userId, apptObj):** Users/Healthcare Providers can view their appointments and details such as date, time, location.
+// Create new appointments
+module.exports.createAppointment = async (userId, date, providerId) => {
+  return await Appointment.create({
+    userId: userId,
+    date: new Date(date), //To Confirm
+    providerId: providerId,
+  });
+};
 
-// - **updateAppt(userId, apptObj):** Only Healthcare Providers can update appointments.
+// Get appointments and details such as date, time, location.
+module.exports.getAppointment = async (userId, isProvider) => {
+  return isProvider
+    ? await Appointment.find({ userId: userId })
+    : await Appointment.find({ providerId: userId });
+};
 
-// - **deleteAppt(userId, apptObj):** Only Healthcare Providers can delete appointments.
+module.exports.updateAppointment = async (apptId, providerId, apptObj) => {
+  // Need to verify that providerId is valid
+  return Appointment.findOneAndUpdate(
+    {
+      _id: apptId,
+      providerId: providerId,
+    },
+    apptObj
+  );
+};
+
+module.exports.cancelAppointment = async (apptId, providerId) => {
+  return await Appointment.deleteOne({
+    _id: apptId,
+    providerId: providerId,
+  });
+};
