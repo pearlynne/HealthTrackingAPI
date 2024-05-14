4fa2c33d45b3e20dbd4e7cc3b7ec7cb25e142a06
pearlynne <chongpearlynnelh@gmail.com>
2024-05-13 17:06:04 -0700

    Add DAO method to get users of providers

--- daos/user.js
+++ daos/user.js
@@ -3,9 +3,22 @@ const User = require("../models/user");
 module.exports = {};
 
 // Store a user record
-module.exports.createUser = async (objLog) => {
+module.exports.signup = async (name, email, hash, roles, providerId) => {
   try {
-    return await User.create(objLog);
+    return providerId
+      ? await User.create({
+          name: name,
+          email: email,
+          password: hash,
+          roles: roles,
+          providerId: providerId,
+        })
+      : await User.create({
+          name: name,
+          email: email,
+          password: hash,
+          roles: roles,
+        }); 
   } catch (e) {
     if (e.message.includes("duplicate")) {
       throw new BadDataError("Email exists");
@@ -20,15 +33,20 @@ module.exports.getUser = async (email) => {
   return await User.findOne({ email: email }).lean();
 };
 
+// Get all user records working with the same provider
+module.exports.getUsersOfProvider = async (providerId) => {
+  return await User.find({ providerId: providerId }).lean();
+};
+
 // Update the user's password field
 module.exports.updateUserPassword = async (userId, password) => {
   return await User.updateOne({ _id: userId }, { password: password });
 };
 
 // Update user’s Healthcare Provider
-module.exports.updateUserProvider = async (userId, provider) => {
-  return await User.updateOne({ _id: userId }, { provider: provider }); 
-	//check if need to change to mongoose object
+module.exports.updateUserProvider = async (userId, providerId) => {
+  return await User.updateOne({ _id: userId }, { providerId: providerId });
+  //check if need to change to mongoose object
 };
 
 class BadDataError extends Error {}
