c80abab4e5d4bb9e42f7e44b11ea9685b1d3c496
pearlynne <chongpearlynnelh@gmail.com>
2024-05-16 15:47:37 -0700

    Add DAO for get appointment by Id and modify date type

--- daos/appointment.js
+++ daos/appointment.js
@@ -3,23 +3,33 @@ const Appointment = require("../models/appointment");
 module.exports = {};
 
 // Create new appointments
-module.exports.createAppointment = async (userId, date, providerId) => {
+module.exports.createAppointment = async (userId, apptDate, providerId) => {
   return await Appointment.create({
     userId: userId,
-    date: new Date(date), //To Confirm
+    date: new Date(apptDate).toDateString, //To Confirm
     providerId: providerId,
   });
 };
 
 // Get appointments and details such as date, time, location.
-module.exports.getAppointment = async (userId, isProvider) => {
+module.exports.getAppointments = async (userId, isProvider) => {
   return isProvider
-    ? await Appointment.find({ userId: userId })
-    : await Appointment.find({ providerId: userId });
+    ? await Appointment.find({ providerId: userId })
+    : await Appointment.find({ userId: userId });
+};
+
+module.exports.getAppointmentById = async (
+  appointmentId,
+  userId,
+  isProvider
+) => {
+  return isProvider
+    ? await Appointment.findOne({ _id: appointmentId, providerId: userId })
+    : await Appointment.findOne({ _id: appointmentId, userId: userId });
 };
 
 module.exports.updateAppointment = async (apptId, providerId, apptObj) => {
-  // Need to verify that providerId is valid
+  // How to update individual appointment variables (for now it's only the date)
   return Appointment.findOneAndUpdate(
     {
       _id: apptId,
