3202e7ba251b56319cc612e16241bbb037b97f6a
pearlynne <chongpearlynnelh@gmail.com>
2024-05-23 15:45:59 -0400

    Revise middleware for testing

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,18 +1,20 @@
 const { Router } = require("express");
 const jwt = require("jsonwebtoken");
+const secret = "t33h33h00";
 
 const userDAO = require("../daos/user");
 
 const isAuthenticated = async (req, res, next) => {
   // How to set bearer token without Postman?
-
   const bearerToken = req.headers.authorization;
+	console.log(bearerToken)
   if (!bearerToken) {
     res.status(401).send("No token");
   } else {
     const tokenString = bearerToken.split(" ")[1];
     try {
       const decoded = jwt.verify(tokenString, secret);
+			console.log(decoded)
       if (decoded) {
         req.user = decoded;
         next();
