61c0444345a740c490d5542ec5f0765bfb5b7c1a
pearlynne <chongpearlynnelh@gmail.com>
2024-05-30 12:21:37 -0700

    Add error handling for objectId

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.use("/users", require("./user"));
 router.use("/reports", require("./reports"));
 router.use("/appointments", require("./appointments"));
 
+
+router.use((err, req, res, next) => { 
+  if (err.message.includes("Cast to ObjectId failed") || err.message.includes("input must be a 24 character hex string")) {
+    res.status(400).send("Invalid Object Id");
+  } else {
+    res.status(500).send("Something else broke!");
+  }
+});
 module.exports = router;
