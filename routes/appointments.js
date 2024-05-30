2bea179c191b34dab2900dccd2f516890d3383d6
pearlynne <chongpearlynnelh@gmail.com>
2024-05-30 12:24:56 -0700

    Modify error codes and add edge cases

--- routes/appointments.js
+++ routes/appointments.js
@@ -2,33 +2,47 @@ const { Router } = require("express");
 const router = Router({ mergeParams: true });
 
 const appointmentDAO = require("../daos/appointment");
-const userDAO = require("../daos/user")
+const userDAO = require("../daos/user");
 const { isAuthenticated, isProvider } = require("../middleware/middleware");
-
 
 // POST /appointments - stores the appointment information
 router.post("/", isAuthenticated, isProvider, async (req, res, next) => {
   const user = req.user;
-  const { patientId, apptDate, providerId } = req.body;
+  const patientId = req.body.userId;
+  const apptDate = req.body.date;
+  const providerId = req.body.providerId;
 
-  if (!req.body || JSON.stringify(req.body) === "{}") {
+  if (
+    !patientId ||
+    !apptDate ||
+    !providerId ||
+    !req.body ||
+    JSON.stringify(req.body) === "{}"
+  ) {
     res.status(404).send("Missing appointment information");
+    //Is this the right error code?
   } else if (isProvider && providerId !== user._id) {
-    res
-      .status(403)
-      .send("You can only create appointments for yourself or your patients");
+    res.status(404).send("You cannot create appointments for other providers");
   } else {
     try {
       const isPatient = await userDAO.getUsersOfProvider(providerId, patientId);
-			if (isPatient){
-				const newAppointment = await appointmentDAO.createAppointment(
-					patientId,
-					apptDate,
-					providerId
-				);
-					res.json(newAppointment);
-			}
-
+      if (isPatient.length === 0) {
+        res
+          .status(404)
+          .send("You can only create appointments for your patients");
+      } else {
+        const newAppointment = await appointmentDAO.createAppointment(
+          patientId,
+          apptDate,
+          providerId
+        );
+        res.send({
+          _id: newAppointment._id,
+          userId: newAppointment.userId,
+          date: newAppointment.date,
+          providerId: newAppointment.providerId,
+        });
+      }
     } catch (e) {
       next(e);
     }
@@ -37,24 +51,19 @@ router.post("/", isAuthenticated, isProvider, async (req, res, next) => {
 
 // GET /appointments - returns all appointments for their userId. If Healthcare Provider, should get array of appointments from all patients/users
 router.get("/", isAuthenticated, async (req, res, next) => {
-  const { user } = req.user;
+  const user = req.user;
   const isProvider = user.roles.includes("provider");
-  try {
-		//if not patient
-		if (isProvider && user._id !== user.providerId) {
-			const isPatient = await userDAO.getUsersOfProvider(providerId, patientId);
-			if (!isPatient){
-				res
-				.status(403)
-				.send("You can only get appointments for yourself or your patients");
-			}
-		} 
-		const newAppointment = await appointmentDAO.getAppointments(
-			user._id,
-			isProvider
-		);
-		res.json(newAppointment);
 
+  try {
+    const newAppointment = await appointmentDAO.getAppointments(
+      user._id,
+      isProvider
+    );
+    if (newAppointment.length === 0) {
+      res.status(200).send("You do not have any appointments");
+    } else {
+      res.json(newAppointment);
+    }
   } catch (e) {
     next(e);
   }
@@ -63,36 +72,40 @@ router.get("/", isAuthenticated, async (req, res, next) => {
 // GET /appointments/:id - returns the appointment with the provided id and that has their userId. If Healthcare Provider, should get specified appointment.
 
 router.get("/:id", isAuthenticated, async (req, res, next) => {
-  const { user } = req.user;
+  const user = req.user;
   const appointmentId = req.params.id;
   const isProvider = user.roles.includes("provider");
+  
   try {
-		//With 2 filters, Should not get an appointment for a patient that isn't theirs.
     const newAppointment = await appointmentDAO.getAppointmentById(
       appointmentId,
       user._id,
       isProvider
     );
-    res.json(newAppointment);
+		
+    if (newAppointment.length === 0) {
+      res.status(404).send("You do not have this appointment");
+    } else {
+      res.json(newAppointment);
+    }
   } catch (e) {
     next(e);
   }
 });
 
-// PUT /appointments/:id(requires authorization) -  Healthcare Providers can update the appointment with the provided id and that has their userId
-
+// PUT /appointments/:id (requires authorization) -  Healthcare Providers can update the appointment with the provided id and that has their userId
 router.put("/:id", isAuthenticated, isProvider, async (req, res, next) => {
-  const { user } = req.user;
+  const user = req.user;
   const appointmentId = req.params.id;
   const appointmentObj = req.body; //should just be the date
   try {
-		//if their appointment; covered in dao
-			// const isAppointment = await appointmentDAO.getAppointmentById(appointmentId, user._id, true);
-			// if (!isAppointment){
-			// 	res
-			// 	.status(403)
-			// 	.send("You can only update appointments for yourself or your patients");
-			// }
+    //if their appointment; covered in dao
+    // const isAppointment = await appointmentDAO.getAppointmentById(appointmentId, user._id, true);
+    // if (!isAppointment){
+    // 	res
+    // 	.status(403)
+    // 	.send("You can only update appointments for yourself or your patients");
+    // }
     const updatedAppointment = await appointmentDAO.updateAppointment(
       appointmentId,
       user._id,
@@ -106,7 +119,7 @@ router.put("/:id", isAuthenticated, isProvider, async (req, res, next) => {
 
 // DELETE /appointments/:id (requires authorization)  - Healthcare Providers can delete appointment with provided id from specified use
 router.delete("/:id", isAuthenticated, isProvider, async (req, res, next) => {
-  const { user } = req.user;
+  const user = req.user;
   const appointmentId = req.params.id;
   try {
     const deletedAppointment = await appointmentDAO.cancelAppointment(
