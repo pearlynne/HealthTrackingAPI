6784ca652834dbfef33651c565eea079880846ff
pearlynne <chongpearlynnelh@gmail.com>
2024-05-30 22:16:39 -0700

    Add email and name for simplicity

--- models/report.js
+++ models/report.js
@@ -6,6 +6,8 @@ const reportSchema = new mongoose.Schema({
     ref: "users",
     required: true,
   },
+	name: { type: String }, //cannot require true?
+	email: { type: String },
   date: { type: Date, required: true },
   mood: { type: Number, required: true },
   inattentiveness: { type: Number, required: true },
