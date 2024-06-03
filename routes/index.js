5593cba5e85d96210d245cd4cf2af378cc6e9911
pearlynne <chongpearlynnelh@gmail.com>
2024-06-02 18:10:43 -0700

    Add mustache and method override form dependencies

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,20 @@
 const { Router } = require("express");
 const router = Router();
+const {routerAuth, addHeader} = require('./auth');
 
-router.use("/auth", require("./auth"));
+// Modified to pass token for API////
+router.all('*', addHeader); //Comment out for tests; 
+
+router.use("/auth", routerAuth);
 router.use("/users", require("./user"));
 router.use("/reports", require("./reports"));
 router.use("/appointments", require("./appointments"));
 
-
-router.use((err, req, res, next) => { 
-  if (err.message.includes("Cast to ObjectId failed") || err.message.includes("input must be a 24 character hex string")) {
+router.use((err, req, res, next) => {
+  if (
+    err.message.includes("Cast to ObjectId failed") ||
+    err.message.includes("input must be a 24 character hex string")
+  ) {
     res.status(400).send("Invalid Object Id");
   } else {
     res.status(500).send("Something else broke!");
--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require("express");
-
 const routes = require("./routes");
-
+const mustacheExpress = require("mustache-express");
+const methodOverride = require("method-override");
 const server = express();
-server.use(express.json());
 
-server.use(routes);
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+
+// mustache, form related
+server.engine("mustache", mustacheExpress());
+server.set("view engine", "mustache");
+server.use(methodOverride("_method"));
+
+server.use(routes); 
+
+//form related
+server.post("/", (req, res) => {
+  //
+});
+
+server.put("/", (req, res) => {
+  //
+});
+
+server.delete("/", (req, res) => {
+  //
+});
+
 
 module.exports = server;
