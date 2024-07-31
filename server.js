11c531d1d95773574b600a79d247e5fdbfa12fad
pearlynne <pearlynne@peapea-annie-24.local>
2024-07-31 15:30:02 -0700

    Add server for d3 charts

--- server.js
+++ server.js
@@ -1,31 +1,29 @@
 const express = require("express");
+const path = require("path")
 const routes = require("./routes");
 const mustacheExpress = require("mustache-express");
 const methodOverride = require("method-override");
 const server = express();
 
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
 
 // mustache, form related
 server.engine("mustache", mustacheExpress());
 server.set("view engine", "mustache");
+// server.set('views', path.join(__dirname, 'views/mustache')); //this fails?
 server.use(methodOverride("_method"));
+
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
 
 server.use(routes); 
 
-//form related
-server.post("/", (req, res) => {
-  //
-});
+server.use(express.static(path.join(__dirname, 'public')));
 
-server.put("/", (req, res) => {
-  //
+server.get('/d3.min.js', (req, res) => {
+    res.sendFile(path.join(__dirname, 'node_modules/d3/dist/d3.min.js'));
 });
-
-server.delete("/", (req, res) => {
-  //
+server.get('/charts/d3Charts.js', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public/charts/d3Charts.js'));
 });
-
 
 module.exports = server;
