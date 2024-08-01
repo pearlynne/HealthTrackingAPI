288c4177a32e861f1a8f77e163e9d1d0175a3584
pearlynne <pearlynne@peapea-annie-24.local>
2024-07-31 23:41:00 -0700

    Add static file path

--- server.js
+++ server.js
@@ -1,29 +1,32 @@
 const express = require("express");
-const path = require("path")
-const routes = require("./routes");
+const path = require("path");
 const mustacheExpress = require("mustache-express");
 const methodOverride = require("method-override");
+const routes = require("./routes"); 
+
 const server = express();
 
+server.use(express.static(path.join(__dirname, 'public')));
 
-// mustache, form related
 server.engine("mustache", mustacheExpress());
+
+server.set('views', path.join(__dirname, 'views'));
+
 server.set("view engine", "mustache");
-// server.set('views', path.join(__dirname, 'views/mustache')); //this fails?
+
 server.use(methodOverride("_method"));
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
-server.use(routes); 
-
-server.use(express.static(path.join(__dirname, 'public')));
+server.use(routes);
 
 server.get('/d3.min.js', (req, res) => {
     res.sendFile(path.join(__dirname, 'node_modules/d3/dist/d3.min.js'));
 });
+
 server.get('/charts/d3Charts.js', (req, res) => {
-	res.sendFile(path.join(__dirname, 'public/charts/d3Charts.js'));
+    res.sendFile(path.join(__dirname, 'public/charts/d3Charts.js'));
 });
 
 module.exports = server;
